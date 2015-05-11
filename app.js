59788c23eb295798145987a5f380a1f1410b1ced
Nicole Lopez <nmlopez21@gmail.com>
2015-05-11 00:15:48 -0500

    added app_cache.js file and cleaned up app.js

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 var input_array = [10, 5, 2, 20];
 console.log(input_array);
 
-//function that iterates through each 
-
-// var array_manip = function(id, input_array) {
-// }
 var array_map = [];
 for (var i = 0; i < input_array.length; i++) {
 	var val = input_array[i];
@@ -13,12 +9,9 @@ for (var i = 0; i < input_array.length; i++) {
 		factors: []
 	});
 };
-console.log(array_map);
 
 var is_factor = function(num1, num2) {
 	if (num1 % num2 === 0) {
-		console.log(num2 + " is a factor of "+ num1);
-		//factors.push(num2);
 		return true;
 	}
 	else {
@@ -28,24 +21,20 @@ var is_factor = function(num1, num2) {
 
 var get_factors = function() {
 	for (var i = 0; i < array_map.length; i++) {
-		//console.log("array_map[i] is "+ array_map[i])
-		console.log(array_map[i].value);
 		for (var x = 0; x < input_array.length; x++) {
 			if (is_factor(array_map[i].value, input_array[x]) && i != x) {
 				array_map[i].factors.push(input_array[x]);
 			}
 			else {
-				console.log("not a factor or it's the same value")
 			}
-			//console.log("input_array[x] is "+ input_array[x]);
 		};
 	};
 }
 
-get_factors();
-
-
 $( document ).ready(function() {
+
+	get_factors();
+	console.log(array_map);
 
 	var $solution = $('#solution');
 	var $input_array = $('<ul>');
@@ -56,5 +45,4 @@ $( document ).ready(function() {
 	}
 
 	$solution.append($input_array);
-	//array_manip(2, input_array);
 });
---
+++ app_cache.js
@@ -0,0 +1,12 @@
+var isPrime = (function () {
+  var cache = {};
+
+  return function (number) {
+    // 1. if value is present in cache,
+    //    return from cache
+    // 2. if value is not present in cache:
+    //  2.1. compute it
+    //  2.2. store value in cache
+    //  2.3. return comuted value
+  }
+}());
