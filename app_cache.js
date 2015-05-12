713cc00c2f8971a6c312b6c56929de2d2d30e987
Nicole Lopez <nmlopez21@gmail.com>
2015-05-11 22:28:22 -0500

    starting to cache version in app_cache.js

--- app_cache.js
+++ app_cache.js
@@ -1,32 +1,18 @@
-// NEED TO CACHE THIS VERSION... 
-// var isPrime = (function () {
-//   var cache = {};
-
-//   return function (number) {
-//     // 1. if value is present in cache,
-//     //    return from cache
-//     // 2. if value is not present in cache:
-//     //  2.1. compute it
-//     //  2.2. store value in cache
-//     //  2.3. return comuted value
-//   }
-// }());
-
 //initial array
 //instead should take user input
-
-
 var input_array = [10, 5, 2, 20];
 console.log(input_array);
-
 var array_map = [];
-for (var i = 0; i < input_array.length; i++) {
-	var val = input_array[i];
-	array_map.push({
-		value: val,
-		factors: []
-	});
-};
+
+var make_array_map = function() {
+	for (var i = 0; i < input_array.length; i++) {
+		var val = input_array[i];
+		array_map.push({
+			value: val,
+			factors: []
+		});
+	};
+}
 
 var is_factor = function(num1, num2) {
 	if (num1 % num2 === 0) {
@@ -43,20 +29,13 @@ var get_factors = function() {
 			if (is_factor(array_map[i].value, input_array[x]) && i != x) {
 				array_map[i].factors.push(input_array[x]);
 			}
-			else {
-			}
-		};
-	};
+		}
+	}
 }
 
-$( document ).ready(function() {
-
-	get_factors();
-	console.log(array_map);
-
-	var $container = $('#container');
+var input_display = function() {
+	var $input = $('#input');
 	var $input_array = $('<ul class="array">');
-
 	for (var i=0; i < input_array.length; i++) {
 		var $li = $('<li>'+input_array[i]+'</li>');
 		$input_array.append($li);
@@ -64,10 +43,33 @@ $( document ).ready(function() {
 			var $li = $('<li>'+" , "+'</li>')
 			$input_array.append($li);
 		}
-		else {
-			console.log("not the last item in array")
-		}
 	}
+	$input.append($input_array);
+}
 
-	$container.append($input_array);
+var output_display = function() {
+	var $output = $('#output');
+	var $ul = $('<ul>');
+	var results = array_map;
+	for (r in results) {
+		var r_value = results[r].value;
+		var r_factors = results[r].factors;
+		var $li = $('<li>'+r_value+": "+r_factors+'</li>');
+		$ul.append($li);
+		//console.log(r_value+": "+r_factors);
+	}
+	$output.append($ul);
+}
+
+$( document ).ready(function() {
+
+	make_array_map();
+	get_factors();
+	console.log(array_map);
+
+	var $container = $('#container');
+
+	input_display();
+	output_display();
+
 });
